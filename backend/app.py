a3c12bf7d1f4263dd4e75a1f9c741c069c103467
ACCISA <aniss.chalah@gmail.com>
2025-02-01 16:01:59 -0500

    test

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,22 @@
 import streamlit as st
 import logging
+import sys
+
+sys.path.append('streamlit')
+
 from csv_processing import csv_processing
 
+if 'component' not in st.query_params:
+    st.write("missing component param")
+else:
 
-params = st.query_params
+    params = st.query_params
 
-pages = {
-    "csv_processing":csv_processing
-}
+    pages = {
+        "csv_processing":csv_processing
+    }
 
-logging.info("Executing: " + str(params.component))
+    logging.info("Executing: " + str(params.component))
 
-pages[params.component]()
+    pages[params.component]()
 
