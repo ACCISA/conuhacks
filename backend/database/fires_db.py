f4a85e2891ab4c1faed77924a98500af3171ec8b
ACCISA <aniss.chalah@gmail.com>
2025-02-02 10:56:23 -0500

    ni

--- backend/database/fires_db.py
+++ backend/database/fires_db.py
@@ -1,6 +1,9 @@
-from database import fires
+from database.database import fires
 
 
 def log_fire_processing(data):
-    
     fires.insert_one(data)
+
+
+def fetch_past_fires():
+    return fires.find({})
--- backend/database/previous_fires.py
+++
@@ -1,7 +0,0 @@
-
-import pandas as pd
-from database import prev_fires
-
-
-def add_csv_to_prev_fires(data):
-    pass
